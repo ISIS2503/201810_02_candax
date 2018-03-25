78f9535ee70adb1701bfa28bec535cd0b43b984a
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-03-25 11:32:29 -0500

    prints en el cliente

--- entrega2/client.py
+++ entrega2/client.py
@@ -2,12 +2,15 @@
 import paho.mqtt.client as mqtt
 import time 
 
+IP = "172.24.41.153"
+puerto = 8083
+
 #Creates the client
 client = mqtt.Client("C2")
 
 #Connects to the mosquitto server on port 8083
-print("Conectando...")
-client.connect("172.24.41.153", port = 8083)
+print("Conectando a " + IP + " en el puerto "+ str(puerto) + "...")
+client.connect(IP, port = puerto)
 print("Conectado.")
 #Subscribes the client to all the topics defined on node-red flow
 client.subscribe("alarm/high/res1/house1")
