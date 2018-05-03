24e912efedc6048045c734c4b5e12f88d61c268c
Valeria <lv.vanegas10@uniandes.edu.co>
2018-05-03 09:25:17 -0500

    REST passwords

--- REST/candax/rest/passwordsREST_rest.py
+++ REST/candax/rest/passwordsREST_rest.py
@@ -7,70 +7,59 @@ import tornado.httpclient as httpclient
 from requests import post as posty
 import candax.rest as rest
 from candax.auth import jwtauth
+import uuid
 
 LOGGER = logging.getLogger(__name__)
 bucket = 'passwords'
 
-
-# alarm = {'house': ; 'res_unit': ; 'hub': ; 'lock': ; 'date':}
 @jwtauth
 class MainHandler(rest.BaseHandler):
     def initialize(self, db=None):
         self.db = db
 
     @tornado.gen.coroutine
-    def get(self, _, _id=None):
-        # print("MSG: {0}".format(self.application.db is None))
-        if _id is None:
-            objs = yield self.application.db.get_all(bucket)
-        else:
-            objs = yield self.application.db.get(bucket, _id)
-        # self.set_status(403)
+    def get(self, *args, **kwargs):
+        objs = yield self.application.db.get_all(bucket)
         objs = json.dumps(objs)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
         self.write(objs)
 
     @tornado.gen.coroutine
     def post(self, *args, **kwargs):
-        print(self.json_args)
         client = httpclient.AsyncHTTPClient()
         h = {'Content-Type': 'application/json; charset=UTF-8'}
-        request = httpclient.HTTPRequest(url='http://localhost:5000/pwds',
-                                         method='POST',
-                                         body=json.dumps(self.json_args),
-                                         headers=h)
-        # try:
+        request = httpclient.HTTPRequest(url='http://localhost:5000/pwds',method='POST',body=json.dumps(self.json_args),headers=h)
         print(self.json_args)
         response = client.fetch(request)
-        # except Exception as ex:
-        #     raise ex
-        # print("posteando....")
-        # posty('http://172.24.42.47:5000/pwds', json=self.json_args)
-        # print("posteado.")
+        k = str(uuid.uuid1().int)
+        self.json_args['key'] = k
+        yield self.application.db.insert(bucket, self.json_args)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
         self.write(self.json_args)
 
     @tornado.gen.coroutine
-    def put(self, *args):
-        # print("MSG: {0}".format(self.application.db is None))
-        # bucket = 'test'
-        objs = yield self.application.db.update(bucket, self.json_args)
-        # self.set_status(403)
-        print(objs)
-        objs = json.dumps(objs)
+    def put(self, *args, **kwargs):
+        client = httpclient.AsyncHTTPClient()
+        h = {'Content-Type': 'application/json; charset=UTF-8'}
+        request = httpclient.HTTPRequest(url='http://localhost:5000/pwds',method='POST',body=json.dumps(self.json_args),headers=h)
+        print(self.json_args)
+        response = client.fetch(request)
+        k = str(uuid.uuid1().int)
+        self.json_args['key'] = k
+        yield self.application.db.insert(bucket, self.json_args)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
-        self.write(objs)
+        self.write(self.json_args)
 
     @tornado.gen.coroutine
-    def delete(self, _, _id=None):
-        # bucket = 'test'
-        print(_id)
-        if _id is None:
-            # objs = yield self.application.db.get_all(bucket)
-            print('no hay naditaaaaa')
-        else:
-            objs = yield self.application.db.delete(bucket, _id)
-        # self.set_status(403)
-        objs = json.dumps(objs)
+    def delete(self, *args, **kwargs):
+        client = httpclient.AsyncHTTPClient()
+        self.json_args['pass'] = '0000'
+        h = {'Content-Type': 'application/json; charset=UTF-8'}
+        request = httpclient.HTTPRequest(url='http://localhost:5000/pwds',method='POST',body=json.dumps(self.json_args),headers=h)
+        print(self.json_args)
+        response = client.fetch(request)
+        k = str(uuid.uuid1().int)
+        self.json_args['key'] = k
+        yield self.application.db.insert(bucket, self.json_args)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
-        self.write(objs)
+        self.write(self.json_args)
