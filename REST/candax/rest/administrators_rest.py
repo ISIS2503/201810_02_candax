0311f097d9adab9c084b30388ab4e33209bd6264
steff456 <steff456@hotmail.com>
2018-05-03 11:58:31 -0500

    arreglos varios del CRUD

--- REST/candax/rest/administrators_rest.py
+++ REST/candax/rest/administrators_rest.py
@@ -9,7 +9,6 @@ import tornado.web
 import tornado.escape
 import candax.rest as rest
 from candax.auth import jwtauth
-import uuid
 
 LOGGER = logging.getLogger(__name__)
 bucket = 'administrators'
@@ -22,45 +21,35 @@ class MainHandler(rest.BaseHandler):
 
     @tornado.gen.coroutine
     def get(self, _, _id=None):
-        # print("MSG: {0}".format(self.application.db is None))
         if _id is None:
             objs = yield self.application.db.get_all(bucket)
+            self.set_status(201)
         else:
             objs = yield self.application.db.get(bucket, _id)
-        # self.set_status(403)
+            self.set_status(201)
         objs = json.dumps(objs)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
         self.write(objs)
 
-#    alarm = {'house': ; 'res_unit': ; 'hub': ; 'lock': ; 'date':}
     @tornado.gen.coroutine
     def post(self, *args):
-        k = str(uuid.uuid1().int)
-        self.json_args['key'] = k
-        _id = yield self.application.db.insert(bucket, self.json_args)
-        # if self.json_args is not None:
-        #   ret, perm, email, _type = yield self.authenticate('administrador')
-        #   if perm:
-        #     edgarin= aerolinea.Aerolinea.from_json(self.json_args)
-        #     response= yield tm.registrar_aerolinea(edgarin)
-        #     self.set_status(201)
-        #     response = response.json()
-        #   else:
-        #     response = tornado.escape.json_encode(ret)
-        #     self.set_status(403)
-        # else:
-        #   self.set_status(400)
-        #   response = "Error: Content-Type must be application/json"
-        # response = "Unknown"
+        if self.json_args is not None:
+            response = yield self.application.db.insert(bucket, self.json_args)
+            self.set_status(201)
+        else:
+            self.set_status(400)
+            response = "Error: Content-Type must be application/json"
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
-        self.write(_id)
+        self.write(response)
 
     @tornado.gen.coroutine
     def put(self, *args):
-        # print("MSG: {0}".format(self.application.db is None))
-        objs = yield self.application.db.update(bucket, self.json_args)
-        # self.set_status(403)
-        print(objs)
+        if self.json_args is not None:
+            objs = yield self.application.db.update(bucket, self.json_args)
+            self.set_status(201)
+        else:
+            self.set_status(400)
+        # print(objs)
         objs = json.dumps(objs)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
         self.write(objs)
@@ -68,11 +57,12 @@ class MainHandler(rest.BaseHandler):
     @tornado.gen.coroutine
     def delete(self, _, _id=None):
         print(_id)
-        if _id is None:
-            print('no hay naditaaaaa')
-        else:
+        if _id is not None:
             objs = yield self.application.db.delete(bucket, _id)
-        # self.set_status(403)
+            self.set_status(201)
+        else:
+            self.set_status(400)
+            objs = "Error: id undefined"
         objs = json.dumps(objs)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
         self.write(objs)
--- REST/candax/rest/alarms_rest.py
+++ REST/candax/rest/alarms_rest.py
@@ -8,7 +8,6 @@ import datetime
 import tornado.web
 import tornado.escape
 import candax.rest as rest
-import uuid
 from candax.auth import jwtauth
 
 LOGGER = logging.getLogger(__name__)
@@ -23,59 +22,48 @@ class MainHandler(rest.BaseHandler):
 
     @tornado.gen.coroutine
     def get(self, _, _id=None):
-        # print("MSG: {0}".format(self.application.db is None))
         if _id is None:
             objs = yield self.application.db.get_all(bucket)
+            self.set_status(201)
         else:
             objs = yield self.application.db.get(bucket, _id)
-        # self.set_status(403)
+            self.set_status(201)
         objs = json.dumps(objs)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
         self.write(objs)
 
     @tornado.gen.coroutine
     def post(self, *args):
-        k = str(uuid.uuid1().int)
-        self.json_args['key'] = k
-        _id = yield self.application.db.insert(bucket, self.json_args)
-        # if self.json_args is not None:
-        #   ret, perm, email, _type = yield self.authenticate('administrador')
-        #   if perm:
-        #     edgarin= aerolinea.Aerolinea.from_json(self.json_args)
-        #     response= yield tm.registrar_aerolinea(edgarin)
-        #     self.set_status(201)
-        #     response = response.json()
-        #   else:
-        #     response = tornado.escape.json_encode(ret)
-        #     self.set_status(403)
-        # else:
-        #   self.set_status(400)
-        #   response = "Error: Content-Type must be application/json"
-        # response = "Unknown"
+        if self.json_args is not None:
+            response = yield self.application.db.insert(bucket, self.json_args)
+            self.set_status(201)
+        else:
+            self.set_status(400)
+            response = "Error: Content-Type must be application/json"
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
-        self.write(_id)
+        self.write(response)
 
     @tornado.gen.coroutine
     def put(self, *args):
-        # print("MSG: {0}".format(self.application.db is None))
-        # bucket = 'test'
-        objs = yield self.application.db.update(bucket, self.json_args)
-        # self.set_status(403)
-        print(objs)
+        if self.json_args is not None:
+            objs = yield self.application.db.update(bucket, self.json_args)
+            self.set_status(201)
+        else:
+            self.set_status(400)
+        # print(objs)
         objs = json.dumps(objs)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
         self.write(objs)
 
     @tornado.gen.coroutine
     def delete(self, _, _id=None):
-        # bucket = 'test'
         print(_id)
-        if _id is None:
-            # objs = yield self.application.db.get_all(bucket)
-            print('no hay naditaaaaa')
-        else:
+        if _id is not None:
             objs = yield self.application.db.delete(bucket, _id)
-        # self.set_status(403)
+            self.set_status(201)
+        else:
+            self.set_status(400)
+            objs = "Error: id undefined"
         objs = json.dumps(objs)
         self.set_header('Content-Type', 'text/javascript;charset=utf-8')
         self.write(objs)
