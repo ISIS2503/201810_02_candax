426ba96d10c4142e80666ef32796e948dc634974
Matt <jm.contreras10>
2018-05-17 16:52:44 -0500

    Math branch

--- Front/react-boilerplate-master/app/containers/App/index.js
+++ Front/react-boilerplate-master/app/containers/App/index.js
@@ -12,6 +12,7 @@ import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
+import Dashboard from 'containers/DashboardPage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Footer from 'components/Footer';
@@ -36,6 +37,7 @@ export default function App() {
       </Helmet>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/dashboard" component={Dashboard} />
         <Route path="" component={NotFoundPage} />
       </Switch>
     </AppWrapper>
